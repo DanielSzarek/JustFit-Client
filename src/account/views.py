949d0b01b4d6b572fbabc25433c0977adb5937f0
daniel <daniel.szarek98@gmail.com>
2020-03-25 17:07:11 +0100

    Add basic endpoints for clients

--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.filters import SearchFilter, OrderingFilter
 
-# Create your views here.
+from .models import Account
+from .serializers import AccountSerializer
+
+
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([BasicAuthentication])
+def user_properties_view(request):
+    try:
+        account = request.user
+    except Account.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AccountSerializer(account)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = AccountSerializer(account, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAdminUser])
+@authentication_classes([BasicAuthentication])
+def account_retrieve_view(request, pk):
+    try:
+        account = Account.objects.get(pk=pk)
+    except Account.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AccountSerializer(account)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'PUT':
+        serializer = AccountSerializer(account, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class AccountListView(ListAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    authentication_classes(BasicAuthentication,)
+    permission_classes = (IsAdminUser,)
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('email', 'username', 'first_name', 'last_name')
